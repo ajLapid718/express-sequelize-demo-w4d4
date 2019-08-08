d65bb1901202e5d941f5ef30b04a83ac91acf347
ajLapid718 <ajLapid718@gmail.com>
2019-08-08 14:45:55 -0400

    Configure sub routers

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,11 @@
 // This is where we'll set up our apiRouter that will handle all of our different API endpoints;
+
+const router = require('express').Router();
+
+const campusesRouter = require("./campuses");
+const studentsRouter = require("./students");
+
+router.use("/campuses", campusesRouter);
+router.use("/students", studentsRouter);
+
+module.exports = router;
