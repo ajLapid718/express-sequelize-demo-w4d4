6b33da19444cd81f7f48f23d3129663836861210
ajLapid718 <ajLapid718@gmail.com>
2019-08-08 18:00:59 -0400

    Note about force true options object

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const apiRouter = require("./routes");
 
 app.use("/api", apiRouter);
 
-db.sync({ force: true })
+db.sync({ force: true }) // NOTE: this option of "force": true is a destructive operation --- it will drop all of your tables --- so you might need two terminals open if this is passed in --- one for running the application and one for seeding your data;
   .then(() => {
   app.listen(3000, () => {
     console.log("Listening on port 3000!!!");
