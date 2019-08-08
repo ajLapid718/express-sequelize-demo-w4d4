604fc2cb8d77bc0df59d82d0af7129e5bf23ad41
ajLapid718 <ajLapid718@gmail.com>
2019-08-08 14:46:33 -0400

    Mount apiRouter in main app

--- app.js
+++ app.js
@@ -3,6 +3,10 @@
 const express = require('express');
 const app = express();
 
+const apiRouter = require("./routes");
+
+app.use("/api", apiRouter);
+
 app.listen(3000, () => {
   console.log("Listening on port 3000!!!");
 })
