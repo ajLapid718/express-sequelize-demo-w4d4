3166c6980b5d392f1d4b971faa6183a64366ff9c
ajLapid718 <ajLapid718@gmail.com>
2019-08-08 17:37:02 -0400

    Sync database first and then listen for requests from web server

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const app = express();
 const db = require('./database');
 const apiRouter = require("./routes");
 
-db.sync();
 app.use("/api", apiRouter);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000!!!");
+db.sync({ force: true })
+  .then(() => {
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!");
+  })
 })
