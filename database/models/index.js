c91c320944f572ca15b6d2166885b718fd536456
ajLapid718 <ajLapid718@gmail.com>
2019-08-08 17:43:34 -0400

    Fix typo with semicolon

--- database/models/index.js
+++ database/models/index.js
@@ -1,6 +1,6 @@
 // Here is where we will execute database definitions and make associations;
 
-const Campus = require("./campus";)
+const Campus = require("./campus");
 const Student = require("./student");
 
 /* TODO: make associations here */
