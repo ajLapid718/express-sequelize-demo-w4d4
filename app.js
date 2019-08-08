0fd1a995b5aacc47423a14b18a19442e909145ba
ajLapid718 <ajLapid718@gmail.com>
2019-08-08 14:43:00 -0400

    Boot up Express server

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // This will be where we set up our Express application, middleware and modules and all;
+
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000!!!");
+})
