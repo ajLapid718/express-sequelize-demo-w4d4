fe8fba82d41ce42007888b32d1a9ebad817d0582
ajLapid718 <ajLapid718@gmail.com>
2019-08-08 17:42:09 -0400

    Declare models and validations and create barrel file

---
+++ database/models/index.js
@@ -0,0 +1,11 @@
+// Here is where we will execute database definitions and make associations;
+
+const Campus = require("./campus";)
+const Student = require("./student");
+
+/* TODO: make associations here */
+
+module.exports = {
+  Campus,
+  Student
+}
