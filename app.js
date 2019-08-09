064c73ca7ac59600233fea2ecc2e99d1ebc4d505
ajLapid718 <ajLapid718@gmail.com>
2019-08-08 21:10:14 -0400

    Sync database first then configure application

--- app.js
+++ app.js
@@ -6,11 +6,42 @@ const app = express();
 const db = require('./database');
 const apiRouter = require("./routes");
 
-app.use("/api", apiRouter);
+// NOTE: this option of "force": true is a destructive operation --- it will drop all of your tables --- so you might need two terminals open if this is passed in --- one for running the application and one for seeding your data;
 
-db.sync({ force: true }) // NOTE: this option of "force": true is a destructive operation --- it will drop all of your tables --- so you might need two terminals open if this is passed in --- one for running the application and one for seeding your data;
-  .then(() => {
+async function syncDatabase() {
+  await db.sync({ force: true });
+}
+
+function configureApp() {
+  app.use("/api", apiRouter);
+
+  // Error handling;
+  app.use((req, res, next) => {
+    if (path.extname(req.path).length) {
+      const err = new Error('Not found');
+      err.status = 404;
+      next(err);
+    }
+    else {
+      next();
+    }
+  });
+
+  // More error handling;
+  app.use((err, req, res, next) => {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal server error.');
+  });
+
   app.listen(3000, () => {
     console.log("Listening on port 3000!!!");
   })
-})
+}
+
+async function bootApp() {
+  await syncDatabase();
+  await configureApp();
+}
+
+bootApp();
