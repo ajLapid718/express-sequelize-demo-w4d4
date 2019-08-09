74d4d5a707d8213161a7463359252c2b389e4213
ajLapid718 <ajLapid718@gmail.com>
2019-08-08 20:50:34 -0400

    Use the built in method to set the foreign key of the student

--- seed.js
+++ seed.js
@@ -4,15 +4,16 @@ const students = [{firstName: "Allan", lastName: "Abe"}, {firstName: "Brian", la
 
 const campuses = [{name: "Hunter College"}, {name: "NYU"}];
 
-async function populateDatabase() {
-  for (let i = 0; i < students.length; i++) {
-    let currentStudent = students[i];
-    await Student.create(currentStudent);
-  }
-  
+async function populateDatabase() {  
   for (let i = 0; i < campuses.length; i++) {
     let currentCampus = campuses[i];
     await Campus.create(currentCampus);
+  }
+
+  for (let i = 0; i < students.length; i++) {
+    let currentStudent = students[i];
+    let savedStudent = await Student.create(currentStudent);
+    await savedStudent.setCampus(1);
   }
 }
 
